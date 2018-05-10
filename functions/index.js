bf6545bc5853ac589a19a77829b041950f9e7049
derekmma <hi@derek.ma>
2018-05-11 01:48:01 +0800

    send detail info to email

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+
+admin.initializeApp(functions.config().firebase);
 
 const gmailEmail = "hotel.iconia@gmail.com";
 const gmailPassword = "doweb666";
@@ -26,13 +29,26 @@ exports.sendRegCfmEmail = functions.database.ref('/users/{userID}')
 });
 
 function sendRegCfmEmail(email, displayName, dbdata) {
+	// find corresponding banquet name
+	console.log(dbdata.banquet);
+	// admin.database().ref('/banquet/'+dbdata.banquet).once('value').then(function(snapshot) {
+	// var banquetName = snapshot.val().name;
 	const mailOptions = {
 		from: `${APP_NAME} <noreply@firebase.com>`,
 		to: email,
 	};
 	mailOptions.subject = `Registration Confirmation from ${APP_NAME}!`;
-	mailOptions.text = `Dear ${displayName || ''}, \n Thanks for your registration at ${APP_NAME}. \n Here is detailed information about your registration: \n`;
+	var mailText = `Dear ${displayName || ''}, \n Thanks for your registration at ${APP_NAME}. \n Here is detailed information about your registration: \n\n`;
+	mailText += `Banquet Name: ${dbdata.banquetName || dbdata.banquet} \n`;
+	mailText += `Your type: ${dbdata.type || ''} \n`;
+	mailText += `Your meal choice: ${dbdata.meal || ''} \n`;
+	mailText += `Your drink choice: ${dbdata.drink || ''} \n`;
+	mailText += `Your seat number: ${dbdata.seat || 'Not assigned yet (Adminitrator will assign seat later)'} \n\n`;
+	mailText += `For assistant, please contact manager for this banquet: \n`;
+	mailText += `Yufan Zhuang, +852 82522528, zhuang@iconia.com\n`;
+	mailOptions.text = mailText;
 	return mailTransport.sendMail(mailOptions).then(() => {
 		return console.log('New regcfm email sent to:', email);
 	});
+	// });
 }
